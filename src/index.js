60a2dd03e89bed57e322452aece7535296421ee3
Liri <liris@diagnosticrobotics.com>
2019-06-16 16:57:41 +0300

    font awesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './serviceWorker';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
