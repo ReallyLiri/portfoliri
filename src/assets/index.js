f318623a3c02141d0476ddce31ae48834aed474f
Liri <liris@diagnosticrobotics.com>
2019-06-19 08:29:08 +0300

    Added new photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,20 @@
 
 export const ALBUMS = {
-  'paintings': {
-    title: 'Paintings',
+  'miniatures': {
+    title: 'Miniature Paintings',
     preview: 'https://i.imgur.com/FlIQJFv.png',
     images: [
       {src: 'https://i.imgur.com/cQIuKDt.png', horizontal: true},
       {src: 'https://i.imgur.com/FlIQJFv.png', horizontal: true}
     ]
   },
-  'miniatures': {
-    title: 'Miniature Paintings',
+  'paintings': {
+    title: 'Paintings',
     preview: 'https://i.imgur.com/QmS1E1r.jpg',
     images: [
       {src: 'https://i.imgur.com/zJd0XoL.jpg', horizontal: false},
-      {src: 'https://i.imgur.com/QmS1E1r.jpg', horizontal: false}
+      {src: 'https://i.imgur.com/QmS1E1r.jpg', horizontal: false},
+      {src: 'https://i.imgur.com/DtVi74M.jpg', horizontal: false}
     ]
   }
 };
