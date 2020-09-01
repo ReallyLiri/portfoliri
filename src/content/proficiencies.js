2e4b313d38b40d7351eb746260fe8cc19f1a5aec
ReallyLiri <reallyliri@gmail.com>
2020-09-01 12:11:09 +0300

    proficiencies

--- src/content/proficiencies.js
+++ src/content/proficiencies.js
@@ -1,69 +1,70 @@
 export const PROFICIENCIES = {
 
-  'Backend Languages': [
-    {name: 'C', devicon: 'c'},
-    {name: 'C++', devicon: 'cplusplus'},
-    {name: 'C#', devicon: 'csharp'},
-    {name: 'Java', devicon: 'java'},
-    {name: 'Python', devicon: 'python'},
-    {name: 'node.js', devicon: 'nodejs'},
-  ],
+    'Backend Languages': [
+        {name: 'C', devicon: 'c'},
+        {name: 'C++', devicon: 'cplusplus'},
+        {name: 'C#', devicon: 'csharp'},
+        {name: 'Java', devicon: 'java'},
+        {name: 'Python', devicon: 'python'},
+        {name: 'node.js', devicon: 'nodejs'},
+    ],
 
-  'Backend Libraries': [
-    {name: '.NET', devicon: 'dot-net'},
-    {name: 'Entity Framework', devicon: 'dot-net'},
-    {name: 'Flask', svg: 'flask'},
-    {name: 'SQLAlchemy', svg: 'sqlalchemy'},
-    {name: 'Pandas', svg: 'pandas'},
-  ],
+    'Backend Frameworks': [
+        {name: '.NET', devicon: 'dot-net'},
+        {name: 'Entity Framework', devicon: 'dot-net'},
+        {name: 'Flask', svg: 'flask'},
+        {name: 'SQLAlchemy', svg: 'sqlalchemy'},
+        {name: 'Pandas', svg: 'pandas'},
+    ],
 
-  'Frontend Web': [
-    {name: 'javascript', devicon: 'javascript'},
-    {name: 'TypeScript', devicon: 'typescript'},
-    {name: 'Angular', devicon: 'angularjs'},
-    {name: 'React, RN, RNW', devicon: 'react'},
-  ],
+    'Frontend Web': [
+        {name: 'javascript', devicon: 'javascript'},
+        {name: 'TypeScript', devicon: 'typescript'},
+        {name: 'Angular', devicon: 'angularjs'},
+        {name: 'React', devicon: 'react'},
+        {name: 'Redux', svg: 'redux'},
+    ],
 
-  'Frontend Desktop/Mobile': [
-    {name: 'WPF', devicon: 'dot-net'},
-    {name: 'Objective-C', svg: 'objectivec'},
-    {name: 'Android', devicon: 'android'},
-    {name: 'React-Native, RNW', devicon: 'react'},
+    'Frontend Desktop/Mobile': [
+        {name: 'WPF', devicon: 'dot-net'},
+        {name: 'Objective-C', svg: 'objectivec'},
+        {name: 'Android', devicon: 'android'},
+        {name: 'React-Native', devicon: 'react'},
+    ],
 
-  ],
+    'Web Server': [
+        {name: 'NGINX', devicon: 'nginx'},
+        {name: 'uWSGI', svg: 'uwsgi'},
+        {name: 'ASP.NET', devicon: 'dot-net'}
+    ],
 
-  'Web Server': [
-    {name: 'NGINX', devicon: 'nginx'},
-    {name: 'uWSGI', svg: 'uwsgi'},
-    {name: 'ASP.NET', devicon: 'dot-net'}
-  ],
+    'Tools': [
+        {name: 'Docker', devicon: 'docker'},
+        {name: 'Vagrant', svg: 'vagrant'},
+        {name: 'git', devicon: 'git'},
+        {name: 'TFS', devicon: 'visualstudio'},
+        {name: 'bash', svg: 'bash'}
+    ],
 
-  'Tools': [
-    {name: 'Docker', devicon: 'docker'},
-    {name: 'Vagrant', svg: 'vagrant'},
-    {name: 'git', devicon: 'git'},
-    {name: 'TFS', devicon: 'visualstudio'},
-    {name: 'bash', svg: 'bash'}
-  ],
+    'Cloud': [
+        {name: 'Amazon Web Services', devicon: 'amazonwebservices'},
+        {name: 'Google Cloud', svg: 'googlecloud'},
+        {name: 'Kubernetes', svg: 'kubernetes'}
+    ],
 
-  'Cloud': [
-    {name: 'Amazon Web Services', devicon: 'amazonwebservices'},
-    {name: 'Google Cloud', svg: 'googlecloud'},
-    {name: 'Kubernetes', svg: 'kubernetes'}
-  ],
+    'Databases': [
+        {name: 'mySQL', devicon: 'mysql'},
+        {name: 'SQL-Server', svg: 'sqlserver'},
+        {name: 'Redis', devicon: 'redis'},
+        {name: 'rocksdb', svg: 'rocksdb'},
+        {name: 'FireBase', svg: 'firebase'},
+        {name: 'MongoDB', devicon: 'mongodb'},
+        {name: 'Neo4j', svg: 'neo4j'}
+    ],
 
-  'Databases': [
-    {name: 'mySQL', devicon: 'mysql'},
-    {name: 'SQL-Server', svg: 'sqlserver'},
-    {name: 'Redis', devicon: 'redis'},
-    {name: 'rocksdb', svg: 'rocksdb'},
-    {name: 'FireBase', svg: 'firebase'},
-    {name: 'MongoDB', devicon: 'mongodb'},
-    {name: 'Neo4j', svg: 'neo4j'}
-  ],
-
-  'Design': [
-    {name: 'Illustrator', devicon: 'illustrator'},
-    {name: 'Photoshop', devicon: 'photoshop'}
-  ]
+    'Design': [
+        {name: 'Illustrator', devicon: 'illustrator'},
+        {name: 'Photoshop', devicon: 'photoshop'},
+        {name: 'Sketch', svg: 'sketch'}
+    ]
 };
